3f8cb25ab9b589dfdcd2dd3632e1da51dd49ccc0
Matteo Pagani <matteo.pagani@microsoft.com>
2021-08-20 14:21:39 +0200

    Add project files.

---
+++ LocalHttpServer.Client/LocalHttpServer.Client/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using LocalHttpServer.Shared;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LocalHttpServer.Client
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+            HttpClient client = new HttpClient();
+            var result = await client.GetStringAsync("http://localhost:5000/WeatherForecast");
+            var forecast = JsonConvert.DeserializeObject<List<WeatherForecast>>(result);
+            ForecastList.ItemsSource = forecast;
+            
+        }
+    }
+}
